2bebf35623713013a4319f08473126c1340908ff
Sergio Navarrete <sergionvte11@outlook.com>
2025-03-25 00:01:43 -0600

    Set DJANGO_SETTINGS_MODULE in build script for environment configuration

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 export PORT=8000
+export DJANGO_SETTINGS_MODULE="projectly.settings"
 
 python manage.py collectstatic --noinput
 python manage.py migrate
