711031673a64a938cb7c65550ba02fe571770f3a
Sergio Navarrete <sergionvte11@outlook.com>
2025-03-25 00:41:05 -0600

    Update allowed hosts and add CSRF trusted origins for production deployment

--- projectly/settings.py
+++ projectly/settings.py
@@ -27,7 +27,9 @@ DEBUG = 'RENDER' not in os.environ
 
 PORT = os.getenv("PORT", "8000")
 
-ALLOWED_HOSTS = ["*", '52.41.36.82', '54.191.253.12', '44.226.122.3']  # Esto es solo para pruebas, luego configúralo bien
+ALLOWED_HOSTS = ["*", '52.41.36.82', '54.191.253.12', '44.226.122.3', "projectly-9qk1.onrender.com", "localhost", "127.0.0.1"]  # Esto es solo para pruebas, luego configúralo bien
+
+CSRF_TRUSTED_ORIGINS = ["https://projectly-9qk1.onrender.com"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
