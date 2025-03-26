98614085f6b921c754e73699265aceff69d04d7b
Sergio Navarrete <sergionvte11@outlook.com>
2025-03-25 20:31:29 -0600

    Add Cloudinary configuration to build script for media storage

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ pip install -r requirements.txt
 
 export PORT=8000
 export DJANGO_SETTINGS_MODULE="projectly.settings"
+export CLOUD_NAME='dngcfjybb'
+export CLOUDINARY_API_KEY='252969854163175'
+export CLOUDINARY_API_SECRET='GFD9tlW39PFVkk3LH-_YZN5Gmjo'
 
 python manage.py collectstatic --noinput
 python manage.py migrate
