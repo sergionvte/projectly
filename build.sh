611c7cf2a877107bae00e789159c589a24624578
Sergio Navarrete <sergionvte11@outlook.com>
2025-05-07 08:47:11 -0600

    Comment out environment variable exports in build script for clarity

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
-export PORT=8000
-export DJANGO_SETTINGS_MODULE="projectly.settings"
-export CLOUD_NAME='dngcfjybb'
-export CLOUDINARY_API_KEY='252969854163175'
-export CLOUDINARY_API_SECRET='GFD9tlW39PFVkk3LH-_YZN5Gmjo'
+# export PORT=8000
+# export DJANGO_SETTINGS_MODULE="projectly.settings"
+# export CLOUD_NAME='dngcfjybb'
+# export CLOUDINARY_API_KEY='252969854163175'
+# export CLOUDINARY_API_SECRET='GFD9tlW39PFVkk3LH-_YZN5Gmjo'
 
 python manage.py collectstatic --noinput
 python manage.py migrate
