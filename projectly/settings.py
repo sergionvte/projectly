a6772144d48785fbf0ff82e2c7ce18f738e5334d
Sergio Navarrete <sergionvte11@outlook.com>
2025-05-07 08:54:32 -0600

    Refactor build script to uncomment environment variable exports and update database path in settings to use BASE_DIR

--- projectly/settings.py
+++ projectly/settings.py
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'projectly.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/data', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
